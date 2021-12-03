25a778823c15adbc7cc4fd495f29ddf8ea963da8
Reyni Thomas T <reyni.1210@gmail.com>
2021-12-03 04:01:02 -0400

    ✨ Creates movie model

---
+++ app/src/main/java/com/example/peliculatest/model/Movie.java
@@ -0,0 +1,68 @@
+package com.example.peliculatest.model;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    private String overview;
+    private Long releaseDate;
+    private String posterPath;
+    private Double voteAverage;
+
+    public Movie(int id, String title, String overview, Long releaseDate, String posterPath, Double voteAverage) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.posterPath = posterPath;
+        this.voteAverage = voteAverage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Long getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Long releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
