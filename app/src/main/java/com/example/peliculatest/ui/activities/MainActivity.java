fe6ba5f08729667c58b117872fd24041f9e74ebe
Reyni Thomas T <reyni.1210@gmail.com>
2021-12-03 14:51:48 -0400

    ✨ Modifies the activity

--- app/src/main/java/com/example/peliculatest/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/peliculatest/ui/activities/MainActivity.java
@@ -3,14 +3,28 @@ package com.example.peliculatest.ui.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.peliculatest.R;
+import com.example.peliculatest.adapters.MoviePagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 pager;
+    private TabLayout tabLayout;
+    private MoviePagerAdapter moviePagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moviePagerAdapter = new MoviePagerAdapter(this);
+        pager = findViewById(R.id.view_pager);
+        pager.setAdapter(moviePagerAdapter);
+        tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, pager, (tab, position) -> tab.setText(position == 0 ? "Peli" : "Fav")).attach();
+
     }
 }
